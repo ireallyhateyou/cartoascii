fd5e58e0cce613cbe066cd6bd445833c8d2d8771
ireallyhateyou <muslimgauzer420@proton.me>
2025-12-04 18:42:28 +0100

    use shapely

--- load_data.py
+++ load_data.py
@@ -1,34 +1,42 @@
 import json
 import requests
+from shapely.geometry import shape, mapping
+from shapely.ops import unary_union
 
 country_borders = "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_0_countries.geojson"
+
+def process_ring(ring):
+        return [(lat, lon) for lon, lat in list(ring.coords)]
+
+def extract_coords(geom):
+    coords_list = []
+    if geom.geom_type == "Polygon":
+        coords_list.append(process_ring(geom.exterior))
+        for interior in geom.interiors:
+            coords_list.append(process_ring(interior))
+    elif geom.geom_type == "MultiPolygon":
+        for poly in geom.geoms:
+            coords_list.append(process_ring(poly.exterior))
+            for interior in poly.interiors:
+                coords_list.append(process_ring(interior))
+    return coords_list
 
 def download_world_borders():
     data = requests.get(country_borders).json()
     countries = {}
 
-    # load all borders into dict
     for feature in data["features"]:
-        props = feature["properties"]
-        name = props["name"]
-        geometry = feature["geometry"]
-        parts = []
+        name = feature["properties"]["name"]
+        geom = shape(feature["geometry"])
+        countries[name] = geom
 
-        if geometry["type"] == "Polygon":
-            for i, ring in enumerate(geometry["coordinates"]):
-                parts.append([(lat, lon) for lon, lat in ring])
-        elif geometry["type"] == "MultiPolygon":
-            for poly in geometry["coordinates"]:
-                for i, ring in enumerate(poly):
-                    parts.append([(lat, lon) for lon, lat in ring])
-
-        countries[name] = parts
-        
-    # im trying not to go to jail
+    # trying not to go to jail :c
     if "W. Sahara" in countries and "Morocco" in countries:
-        if len(countries["Morocco"]) > 1:
-            countries["Morocco"] = [countries["Morocco"][0]]
-        countries["Morocco"].extend(countries["W. Sahara"])
+        morocco_geom = countries["Morocco"]
+        ws_geom = countries["W. Sahara"]
+        merged_geom = morocco_geom.union(ws_geom) 
+        countries["Morocco"] = merged_geom
         del countries["W. Sahara"]
-        
-    return countries
+
+    countries_coords = {name: extract_coords(geom) for name, geom in countries.items()}
+    return countries_coords
